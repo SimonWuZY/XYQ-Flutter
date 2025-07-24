54481946c7eb365223f61213c3dc0971a1e5b797
Limpid <Limpid8818@outlook.com>
2025-07-24 22:22:09 +0800

    config environ

--- backend/tools/xyqStoryUtils/main.py
+++ backend/tools/xyqStoryUtils/main.py
@@ -4,5 +4,6 @@ if __name__ == "__main__":
     question = input("请输入：\n")
     #results = work_flow(question, True)
     results = workflow(question)
+    #results = prompt_LLM(question)
 
     print(results)
